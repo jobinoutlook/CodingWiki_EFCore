bbefa706fdee122c332c19579b1a29348f552ac6
jobin jose samuel <jobin.samuel@outlook.com>
2026-06-17 18:02:39 +0530

    Fluent Config Example class added

---
+++ CodingWiki_DataAccess/FluentConfig/FluentBookConfig.cs
@@ -0,0 +1,25 @@
+﻿using CodingWiki_Model.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWiki_DataAccess.FluentConfig
+{
+    public class FluentBookConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> modelBuilder)
+        {
+            //modelBuilder.HasData(
+            //    new Book() { BookId = 1, ISBN = "I054485754", Price = (decimal)10.26, Title = "Harry Potter", PublisherId = 1 },
+            //    new Book() { BookId = 2, ISBN = "I954T56565", Price = (decimal)20.26, Title = "Super Mario", PublisherId = 2 },
+            //    new Book() { BookId = 3, ISBN = "I867433467", Price = (decimal)30.26, Title = "He Man", PublisherId = 3 },
+            //    new Book() { BookId = 4, ISBN = "I056665754", Price = (decimal)40.26, Title = "Spider Man", PublisherId = 3 }
+            //    );
+        }
+    }
+}
---
+++ CodingWiki_DataAccess/FluentConfig/FluentBookDetailConfig.cs
@@ -0,0 +1,27 @@
+﻿using CodingWiki_Model.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWiki_DataAccess.FluentConfig
+{
+    public class FluentBookDetailConfig : IEntityTypeConfiguration<BookDetail>
+    {
+        public void Configure(EntityTypeBuilder<BookDetail> modelBuilder)
+        {
+            
+
+            //modelBuilder.HasData(
+
+            //    new BookDetail() { BookDetailId = 1, BookId = 1, NumberOfChapters = 10, NumberOfPages = 100, Weight = "100" },
+            //    new BookDetail() { BookDetailId = 2, BookId = 2, NumberOfChapters = 20, NumberOfPages = 200, Weight = "200" },
+            //    new BookDetail() { BookDetailId = 3, BookId = 3, NumberOfChapters = 30, NumberOfPages = 300, Weight = "300" },
+            //    new BookDetail() { BookDetailId = 4, BookId = 4, NumberOfChapters = 40, NumberOfPages = 400, Weight = "400" }
+            //    );
+        }
+    }
+}
